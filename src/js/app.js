1371c4ff114cca01f4ad5acc671e4fa32d597736
IdaHolmen <143997448+IdaHolmen@users.noreply.github.com.>
2024-02-05 09:49:26 +0100

    Changed file structure

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+
+
+const getFilmData = async () => {
+	const response = await fetch('https://swapi.dev/api/films/');
+	const data = await response.json()
+
+	const films = data.results;
+	console.log(films);
+}
+getFilmData()
+
+
